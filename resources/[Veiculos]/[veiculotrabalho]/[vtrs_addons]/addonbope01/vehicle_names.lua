7380c149508e9abb7d4104f653ec9413001e7cf0
Leandro Dayrell Silva <leandrodayrell@live.com>
2018-02-24 18:02:19 +0000

    2.0
    2.0

--- resources/[Veiculos]/[veiculotrabalho]/[vtrs_addons]/addonbope01/vehicle_names.lua
+++
@@ -1,8 +0,0 @@
-function AddTextEntry(key, value)
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
-end
-
-Citizen.CreateThread(function()
-  AddTextEntry('0xC593CAF5', 'Nissan Pathfinder BOPE')
-  AddTextEntry('0xC990C46A', 'Nissan Pathfinder BOPE')
-end)
--- resources/[Veiculos]/[veiculotrabalho]/[vtrs_addons]/addonbope02/vehicle_names.lua
+++
@@ -1,8 +0,0 @@
-function AddTextEntry(key, value)
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
-end
-
-Citizen.CreateThread(function()
-  AddTextEntry('0xC593CAF5', 'Volkswagen Arteon BOPE')
-  AddTextEntry('0xC990C46A', 'Volkswagen Arteon BOPE')
-end)
