94b0ad9bc7ca29530fcdd6f47a07defc7ef932a7
Leandro Dayrell Silva <leandrodayrell@live.com>
2018-02-26 01:27:11 +0000

    2.4
    2.4

--- [plugin]/npc_control/cfg/npcs.lua
+++ [plugin]/npc_control/cfg/npcs.lua
@@ -1,8 +1,8 @@
 cfg = {}
 
 cfg.density = {
-	peds = 4,
-	vehicles = 5
+	peds = 7,
+	vehicles = 7
 }
 
 cfg.peds = { -- these peds wont show up anywhere, they will be removed and their vehicles deleted
