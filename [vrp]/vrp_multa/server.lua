4dccf1f7f34c2192fa8416cfad8749697e64cc52
Leandro Dayrell Silva <leandrodayrell@live.com>
2018-03-03 01:18:46 -0300

    Atualizado VRP_Multa
    Adicionado tunnel vrpex

--- [vrp]/vrp_multa/server.lua
+++ [vrp]/vrp_multa/server.lua
@@ -1,3 +1,23 @@
+
+local Tunnel = module("vrp", "lib/Tunnel")
+local Proxy = module("vrp", "lib/Proxy")
+
+-- load global and local languages
+local Luang = module("vrp", "lib/Luang")
+local Lang = Luang()
+Lang:loadLocale(lcfg.lang, module("vrp", "cfg/lang/"..lcfg.lang) or {})
+Lang:loadLocale(lcfg.lang, module("vrp_multa", "cfg/lang/"..lcfg.lang) or {})
+lang = Lang.lang[lcfg.lang]
+
+vRP = Proxy.getInterface("vRP")
+vRPclient = Tunnel.getInterface("vRP")
+Mclient = Tunnel.getInterface("vrp_multa")
+
+
+
+
+
+
 local Tunnel = module("vrp", "lib/Tunnel")
 local Proxy = module("vrp", "lib/Proxy")
 
@@ -6,8 +26,14 @@ vRPclient = Tunnel.getInterface("vRP","vrp_multa")
 
 RegisterServerEvent('cobrarMulta')
 AddEventHandler('cobrarMulta', function()
-	local user_id = vRP.getUserId({source})
-	local player = vRP.getUserSource({user_id})
-	local multa = 1000
-	vRP.tryFullPayment({user_id,multa})
+
+	local user_id = vRP.getUserId(source)
+	local player = vRP.getUserSource(user_id)
+	local cops = vRP.getUsersByPermission(cfg.cops)
+	local multa = 99
+    if vRP.hasPermission(user_id,cfg.cops) then
+    else
+	vRP.tryFullPayment(user_id,multa)
+	vrp
+end
 end)
