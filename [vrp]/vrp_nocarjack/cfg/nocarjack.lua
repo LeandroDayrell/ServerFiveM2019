2276efcc7bba762e159d216bbfd5bc26d68cd47d
Leandro Dayrell Silva <leandrodayrell@live.com>
2018-03-06 19:06:44 -0300

    att
    att

--- [vrp]/vrp_nocarjack/cfg/nocarjack.lua
+++ [vrp]/vrp_nocarjack/cfg/nocarjack.lua
@@ -18,7 +18,7 @@
 
 cfg = {}
 
-cfg.chance = 80 -- chance of being unlocked in percentage
+cfg.chance = 10 -- chance of being unlocked in percentage
 
 cfg.blacklist = { -- vehicles that will always be locked when spawned naturally
   "T20",
